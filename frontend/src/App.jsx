6cdd1e623fa503ed7bda5f785dcad2a4c1c217ce
quangthoIT <quangthoIT04@gmail.com>
2025-10-04 01:28:28 +0700

    Page NotFound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster} from "sonner";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // npm install react-router-dom
+import { Toaster} from "sonner"; // npm install sonner
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react"; // npm install lottie-react
+import error404 from "../assets/Error 404.json";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-slate-50">
+      <Lottie animationData={error404} loop={true} className="w-96 h-96 sm:w-[560px] sm:h-[560px]" />
 
-export default NotFound
+      <a
+        href="/"
+        className="inline-block px-5 py-3 mt-5 font-medium text-white bg-[#7C3AED] transition shadow-md rounded-xl hover:bg-[#5A1FCB]"
+      >
+        Quay về trang chủ
+      </a>
+    </div>
+  );
+};
+
+export default NotFound;
