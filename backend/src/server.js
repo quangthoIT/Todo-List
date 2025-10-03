d58a7019aeaa48bc14a9a73d9a530989ffd7a956
quangthoIT <quangthoIT04@gmail.com>
2025-10-03 23:27:30 +0700

    Kết nối DB trước khi chạy Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,16 +6,17 @@ import { connectDB } from "./config/db.js";
 
 dotenv.config(); // Đọc file .env
 
-connectDB(); // Kết nối đến database
-
 const PORT = process.env.PORT || 5001; // Đọc biến PORT tạo trong file .env hoặc 5001
 
 const app = express(); // Khởi tạo express
 
-app.use(express.json());
+app.use(express.json()); // Khóa xây dựng JSON
 
 app.use("/api/tasks", taskRouter); // Khởi tạo router
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Kết nối với cơ sở dữ liệu trước khi chạy server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
