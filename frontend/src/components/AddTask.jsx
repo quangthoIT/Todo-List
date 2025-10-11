387f88ebd4e12311457d15957bb120c7dc67596a
quangthoIT <quangthoIT04@gmail.com>
2025-10-11 09:42:46 +0700

    Thêm nhiệm vụ, api axios, khóa thêm khi không nhập nhiệm vụ

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "./ui/card";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
+import { toast } from "sonner";
+import api from "@/lib/axios";
 
 const AddTask = () => {
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const addTask = async () => {
+    if (newTaskTitle.trim()) {
+      try {
+        await api.post("/tasks", {
+          title: newTaskTitle,
+        });
+        toast.success(`Nhiệm vụ ${newTaskTitle} đã được thêm vào.`);
+        setNewTaskTitle("");
+      } catch (error) {
+        console.error("Lỗi xảy ra khi thêm nhiệm vụ:", error);
+        toast.error("Lỗi xảy ra khi thêm nhiệm vụ.");
+      }
+      setNewTaskTitle("");
+    } else {
+      toast.error("Vui lòng nhập nội dung nhiệm vụ.");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
   return (
     <Card className="p-6 border-0 bg-slate-50 shadow-md mx-5 sm:mx-0">
       <div className="flex flex-col gap-4 sm:flex-row">
         <Input
           type="text"
           placeholder="Nhập nhiệm vụ"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+          onKeyPress={handleKeyPress}
           className="h-12 bg-slate-100 sm:flex-1 border border-slate-300 shadow-sm focus:border-slate-500  focus:ring-slate-200"
         />
 
-        <Button variant="mine" size="xl">
+        <Button
+          variant="mine"
+          size="xl"
+          onClick={addTask}
+          disabled={!newTaskTitle}
+        >
           <Plus />
           Thêm
         </Button>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import StartAndFilters from "@/components/StartAndFilters";
 import TaskList from "@/components/TaskList";
 import TaskListPagination from "@/components/TaskListPagination";
 import { toast } from "sonner";
-import axios from "axios";
+import api from "@/lib/axios";
 
 const HomePage = () => {
   const [taskBuffer, setTaskBuffer] = useState([]);
@@ -24,7 +24,7 @@ const HomePage = () => {
 
   const fetchTasks = async () => {
     try {
-      const res = await axios.get("http://localhost:5001/api/tasks");
+      const res = await api.get("/tasks");
       setTaskBuffer(res.data.tasks);
       setActiveTaskCount(res.data.activeCount);
       setCompletedTaskCount(res.data.completedCount);
@@ -45,6 +45,10 @@ const HomePage = () => {
         return true;
     }
   });
+
+  const handleTaskChanged = () => {
+    fetchTasks();
+  };
 
   return (
     <div className="min-h-screen w-full relative">
@@ -72,7 +76,7 @@ const HomePage = () => {
             {/* Đầu trang */}
             <Header />
             {/* Tạo task */}
-            <AddTask />
+            <AddTask handleTaskChanged={handleTaskChanged} />
             {/* Thống kê và Bộ lọc */}
             <StartAndFilters
               filter={filter}
